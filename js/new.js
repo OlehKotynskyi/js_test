1d8ae53fceb0b379b2e026fc96b7531fd7279006
Oleh Kot <olegkotynski@gmail.com>
2023-02-01 08:48:20 +0200

    new commit one
    перша частина уроку , додали код з питаннями до користовача про фільми

--- js/new.js
+++ js/new.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let namber = 5;
-const leftBorderWidth = 1;  
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   mavies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+};
+
+const a = prompt('Один из последних фильмов?', ''),
+   b = prompt('На сколько оцените его?', ''),
+   c = prompt('Один из последних фильмов?', ''),
+   d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.mavies[a] = b;
+personalMovieDB.mavies[c] = d;
+
+console.log(personalMovieDB);
