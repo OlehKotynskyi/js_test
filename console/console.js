236c7ff3e20de3c4b01decc5bfb7616b93062e9a
Oleh Kot <olegkotynski@gmail.com>
2023-02-04 21:32:31 +0200

    Виконання логічних операторів та циклів

---
+++ console/console.js
@@ -0,0 +1,108 @@
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+//const hamburger = 3;
+//const fries = 3;
+//const cola = 0;
+//const nuggets = 2;
+
+
+//if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//   console.log('Done!')
+//}
+
+
+//let hamburger;
+//const fries = NaN;
+//const cola = 0;
+//const nuggets = 2;
+
+
+//if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log('Done!')
+//}
+
+//let hamburger;
+//const fries = NaN;
+//const cola = 0;
+//const nuggets = 2;
+
+
+//if (hamburger && cola || fries === 3 && nuggets) {
+//   console.log('Done!')
+//}
+
+
+// Пишем решение вот тут
+//for (let i = 5; i <= 10; i++) {
+//   console.log(i);
+//}
+
+
+//stop: for (let i = 20; i >= 10; i--) {
+//   if (i === 13) {
+//      break stop;
+//   }
+//   console.log(i);
+//}
+
+//for (let i = 2; i <= 10; i +=2) {
+//   console.log(i);
+//}
+
+//for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//      continue;
+//   } else {
+//      console.log(i);
+//   }
+//}
+
+//let result = '';
+//let i = 1;
+
+//do {
+//   i += 1;
+//   if (i % 2 === 0) {
+//      continue;
+//   }
+//   result += i + '\n';
+//} while (i < 16); 
+
+//console.log(result);
+
+//let j = 2;
+
+//while (j <= 16) {
+//   if (j % 2 === 0) {
+//      j++;
+//      continue;
+//   } else {
+//      console.log(j);
+//   }
+//   j++;
+//}
+
+
+//const arrayyOfNumbers = [];
+
+//for (let i = 5; i < 11; i++) {
+//   arrayyOfNumbers[i - 5] = i;
+//}
+
+//console.log(arrayyOfNumbers);
+//return arrayyOfNumbers;
+
